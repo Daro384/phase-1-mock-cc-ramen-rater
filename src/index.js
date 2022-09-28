1eaaf04a8381a393dc28dc53edc7f63ff4cfb824
Joseph <joffan98@gmail.com>
2022-09-28 18:19:50 -0500

    made a speedrunBranch

--- src/index.js
+++ src/index.js
@@ -1,90 +1,93 @@
+//time start = 4:58
+
 const API = "http://localhost:3000/ramens"
 
-const showRamen = (ramenObject) => {
-const centerIMG = document.querySelector("#ramen-detail img")
-    centerIMG.src = ramenObject.image
-    centerIMG.alt = ramenObject.name
-    centerIMG.value = ramenObject.id
-
-    const ramenName = document.querySelector("h2.name")
-    ramenName.textContent = ramenObject.name
-
-    const ramenResturant = document.querySelector("h3.restaurant")
-    ramenResturant.textContent = ramenObject.restaurant
-
-    const ramenRating = document.getElementById("rating-display")
-    ramenRating.textContent = ramenObject.rating
-
-    const ramenComment = document.getElementById("comment-display")
-    ramenComment.textContent = ramenObject.comment
-}
-
-const addRamen = ramenObject => {
+const addingRamenIMG = ramenObject => {
     const ramenIMG = document.createElement("img")
     ramenIMG.src = ramenObject.image
-    ramenIMG.id = "img" + ramenObject.id.toString()
-    
+    ramenIMG.alt = ramenObject.name
+    ramenIMG.id = "IMG " + ramenObject.id
     document.getElementById("ramen-menu").append(ramenIMG)
-    ramenIMG.addEventListener("click", () => {
-        showRamen(ramenObject)
-    })
+
+    ramenIMG.addEventListener("click", () => displayRamen(ramenObject))
 }
 
-const submitRamen = () => {
-    const ramenForm = document.getElementById("new-ramen")
-    ramenForm.addEventListener("submit", event => {
+const displayRamen = ramenObject => {
+    const ramenIMG = document.querySelector("img.detail-image")
+    ramenIMG.src = ramenObject.image
+    ramenIMG.alt = ramenObject.name
+    ramenIMG.value = ramenObject.id //used to know which ramen to delete when deleteButton is pressed
+
+    document.querySelector("div h2.name").textContent = ramenObject.name
+    document.querySelector("div h3.restaurant").textContent = ramenObject.restaurant
+    document.getElementById("rating-display").textContent = ramenObject.rating
+    document.getElementById("comment-display").textContent = ramenObject.comment
+}
+
+
+
+const postRamen = (url, ramenObject) => {
+    const data = {
+        method:"post",
+        headers: {
+            "content-type":"application/json",
+            "accept":"application/json"
+        },
+        body: JSON.stringify(ramenObject)
+    }
+    return fetch(url, data) //spent 15 minutes wondering why it didnt work (i forgot the return)
+}
+
+const addRamenForm = () => {
+    document.getElementById("new-ramen").addEventListener("submit", event => {
         event.preventDefault()
-
-        const newRamenObject = {}
-        newRamenObject["name"] = document.getElementById("new-name").value
-        newRamenObject["resturant"] = document.getElementById("new-restaurant").value
-        newRamenObject["image"] = document.getElementById("new-image").value
-        newRamenObject["rating"] = document.getElementById("new-rating").value
-        newRamenObject["comment"] = document.getElementById("new-comment").value
-
-        fetch(API)
-        .then(resp => resp.json())
+        const newRamenObject = {
+            name:event.target.name.value,
+            restaurant:event.target.restaurant.value,
+            image:event.target.image.value,
+            rating:event.target.rating.value,
+            comment: event.target["new-comment"].value
+        }
+        fetch(API).then(resp => resp.json())
         .then(ramenArray => {
-            newRamenObject.id = ramenArray.slice(-1)[0].id + 1
-            const postData = {
-                method:"post",
-                headers: {
-                    "content-type":"application/json",
-                    "accept":"application/json"
-                },
-                body: JSON.stringify(newRamenObject)
-            }
-            addRamen(newRamenObject)
-            fetch(API, postData)
+            newRamenObject["id"] = ramenArray.length + 1
+            postRamen(API, newRamenObject).then(() => {
+                addingRamenIMG(newRamenObject)
+            })
+            
         })
     })
 }
 
 const renderPage = () => {
-    fetch(API)
-    .then(resp => resp.json())
-    .then(objectArray => {
-        objectArray.forEach(object => {
-            addRamen(object)
+    fetch(API).then(resp => resp.json())
+    .then(ramenObjects => {
+        ramenObjects.forEach(ramenObject => {
+            addingRamenIMG(ramenObject)
         })
-        showRamen(objectArray[0])
+        displayRamen(ramenObjects[0])
     })
 }
 
-const theDeleteButton = () => {
-    const deleteButton = document.getElementById("delete")
-    deleteButton.addEventListener("click", () => {
-        const currentId = document.querySelector("#ramen-detail img").value
-        fetch(`${API}/${currentId}`,{method:"delete"})
+const deleteRamenButton = () => {
+    const deleteButton = document.createElement("button")
+    deleteButton.textContent = "delete"
+    deleteButton.addEventListener("click", event => {
+        const ramenId = document.querySelector("img.detail-image").value
+        fetch(API + "/" + ramenId, {method:"delete"})
         .then(() => {
-            document.getElementById("img" + currentId.toString()).remove()
-            fetch(API).then(resp => resp.json())
-            .then(objectArray => showRamen(objectArray[0]))
+            document.getElementById("IMG "+ ramenId).remove()
+            fetch(API).then(res => res.json())
+            .then(ramenObjects => displayRamen(ramenObjects[0]))
         })
         
     })
+    document.getElementById("ramen-detail").append(deleteButton)
 }
 
 renderPage()
-submitRamen()
-theDeleteButton()
+addRamenForm()
+deleteRamenButton()
+
+
+//done 6:14 finished in 76 minutes
