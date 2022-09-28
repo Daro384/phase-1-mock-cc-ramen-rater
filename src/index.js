31e29ddc3109edb8e59c93d47fc1ca0674ea4b2f
Joseph <joffan98@gmail.com>
2022-09-28 13:55:38 -0500

    extra Advanced Deliverables

--- src/index.js
+++ src/index.js
@@ -1,52 +1,62 @@
 // write your code here
-const ramen1 = {
-    id: 1,
-    name: "Shoyu Ramen",
-    restaurant: "Nonono",
-    image: "./assets/ramen/shoyu.jpg",
-    rating: 7,
-    comment: "Delish. Can't go wrong with a classic!"
-  }
+const showRamen = (ramenObject) => {
+    const mainIMG = document.querySelector("#ramen-detail img")
+    mainIMG.src = ramenObject.image
+    mainIMG.alt = ramenObject.id
+
+    const ramenName = document.querySelector("h2.name")
+    ramenName.textContent = ramenObject.name
+
+    const ramenResturant = document.querySelector("h3.restaurant")
+    ramenResturant.textContent = ramenObject.restaurant
+
+    const ramenRating = document.getElementById("rating-display")
+    ramenRating.textContent = ramenObject.rating
+
+    const ramenComment = document.getElementById("comment-display")
+    ramenComment.textContent = ramenObject.comment
+}
+
 const addRamen = ramenObject => {
     const ramenIMG = document.createElement("img")
     ramenIMG.src = ramenObject.image
+    ramenIMG.id = "img" + ramenObject.id.toString()
     
     document.getElementById("ramen-menu").append(ramenIMG)
-    ramenIMG.addEventListener("click", (event) => {
-
-        const mainIMG = document.querySelector("#ramen-detail img")
-        mainIMG.src = ramenObject.image
-        mainIMG.alt = ramenObject.name
-
-        const ramenName = document.querySelector("h2.name")
-        ramenName.textContent = ramenObject.name
-
-        const ramenResturant = document.querySelector("h3.restaurant")
-        ramenResturant.textContent = ramenObject.restaurant
-
-        const ramenRating = document.getElementById("rating-display")
-        ramenRating.textContent = ramenObject.rating
-
-        const ramenComment = document.getElementById("comment-display")
-        ramenComment.textContent = ramenObject.comment
+    ramenIMG.addEventListener("click", () => {
+        showRamen(ramenObject)
     })
 }
 
-const ramenForm = document.getElementById("new-ramen")
-ramenForm.addEventListener("submit", event => {
-    event.preventDefault()
+const submitRamen = () => {
+    const ramenForm = document.getElementById("new-ramen")
+    ramenForm.addEventListener("submit", event => {
+        event.preventDefault()
 
-    const newRamenObject = {}
-    newRamenObject["name"] = document.getElementById("new-name").value
-    newRamenObject["resturant"] = document.getElementById("new-restaurant").value
-    newRamenObject["image"] = document.getElementById("new-image").value
-    newRamenObject["rating"] = document.getElementById("new-rating").value
-    newRamenObject["comment"] = document.getElementById("new-comment").value
-    newRamenObject.id = document.querySelectorAll("div#ramen-menu > img").lenght
+        const newRamenObject = {}
+        newRamenObject["name"] = document.getElementById("new-name").value
+        newRamenObject["resturant"] = document.getElementById("new-restaurant").value
+        newRamenObject["image"] = document.getElementById("new-image").value
+        newRamenObject["rating"] = document.getElementById("new-rating").value
+        newRamenObject["comment"] = document.getElementById("new-comment").value
 
-    addRamen(newRamenObject)
-    
-})
+        fetch("http://localhost:3000/ramens")
+        .then(resp => resp.json())
+        .then(ramenArray => {
+            newRamenObject.id = ramenArray.slice(-1)[0].id + 1
+            const postData = {
+                method:"post",
+                headers: {
+                    "content-type":"application/json",
+                    "accept":"application/json"
+                },
+                body: JSON.stringify(newRamenObject)
+            }
+            addRamen(newRamenObject)
+            fetch("http://localhost:3000/ramens", postData)
+        })
+    })
+}
 
 const renderPage = () => {
     fetch("http://localhost:3000/ramens")
@@ -55,7 +65,24 @@ const renderPage = () => {
         objectArray.forEach(object => {
             addRamen(object)
         })
+        showRamen(objectArray[0])
+    })
+}
+
+const theDeleteButton = () => {
+    const deleteButton = document.getElementById("delete")
+    deleteButton.addEventListener("click", () => {
+        const currentId = document.querySelector("#ramen-detail img").alt
+        fetch(`http://localhost:3000/ramens/${currentId}`,{method:"delete"})
+        .then(() => {
+            document.getElementById("img" + currentId.toString()).remove()
+            fetch("http://localhost:3000/ramens").then(resp => resp.json())
+            .then(objectArray => showRamen(objectArray[0]))
+        })
+        
     })
 }
 
 renderPage()
+submitRamen()
+theDeleteButton()
